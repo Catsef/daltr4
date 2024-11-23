aad14bc31bbbd94a36b10f4fef93b88ab5925fb8
caltr4 <catieman2120@gmail.com>
2024-11-23 17:39:04 +0800

    backwardpass

--- daltr4/Program.cs
+++ daltr4/Program.cs
@@ -4,10 +4,16 @@ using daltr4;
 
 Console.WriteLine("hello bro - Made by Caltr4 - https://github.com/Catsef/daltr4");
 
-
-
-public class DenseLayer
+public class Activation
 {
+    private activationFunction ActivationFunction;
+    
+    
+    
+    public Activation(activationFunction func)
+    {
+        ActivationFunction = func;
+    }
     
     double ReLU (double x)
     {
@@ -17,23 +23,54 @@ public class DenseLayer
     {
         return 2/(1+Math.Pow(Math.E, -(2*x)))-1;
     }
-    double BipolarSigmoid(double x)
+    private double BipolarSigmoid(double x)
     {
         return (1-Math.Exp(-x))/(1+Math.Exp(-x));
     }
-    double activate(double x)
+
+    private double UnReLU(double x)
+    {
+        if (x < 0) return 0;
+        else return 1;
+    }
+
+    private double UnTanh(double x)
+    {
+        double e = Math.E;
+        return 1 - Math.Pow(Math.Exp(x) - Math.Exp(-x), 2) / Math.Pow(Math.Exp(x) + Math.Exp(-x), 2);
+    }
+
+    private double UnBipolarSigmoid(double x)
+    {
+        return 1 - Math.Pow(Tanh(x / 2), 2);
+    }
+    public double activate(double x)
     {
         if (ActivationFunction == activationFunction.ReLu) return ReLU(x);
         if (ActivationFunction == activationFunction.Tanh) return Tanh(x);
         if (ActivationFunction == activationFunction.Sigmoid) return BipolarSigmoid(x);
         else return 0;
     }
+
+    public double derivactivate(double x)
+    {
+        if (ActivationFunction == activationFunction.ReLu) return UnReLU(x);
+        if (ActivationFunction == activationFunction.Tanh) return UnTanh(x);
+        if (ActivationFunction == activationFunction.Sigmoid) return UnBipolarSigmoid(x);
+        else return 0;
+    }
+}
+
+public class DenseHiddenLayer
+{
     
-    private int InputSize; // previous layer's size or CNN input size
-    private int OutputSize;
+ 
+    
+    private int InputSize; // previous layer's size
+    private int OutputSize; // next layer's size
     private int Size;
     private double LearningRate; // tWEAKUNG SIZE
-    private activationFunction ActivationFunction;
+    private Activation Activation;
     private double[][] weights; // weights [Input] [Neuron]
     private double[] biases;
 
@@ -45,14 +82,14 @@ public class DenseLayer
             weights[x] = new double[Size];
             for (var i = 0; i < Size; i++)
             {
-                weights[x][i] = RandomNumberGenerator.GetInt32(-1, 1);
+                weights[x][i] = RandomNumberGenerator.GetInt32(0, 1);
             }
         }
         biases = new double[Size]; // its the intercept - offsets values. Neuron V = (Previous Layer : Sum : ReLU * weights) + bias.
 
         for (var i = 0; i < Size; i++)
         {
-            biases[i] = RandomNumberGenerator.GetInt32(-1, 1);
+            biases[i] = RandomNumberGenerator.GetInt32(0, 1);
         }
     }
     
@@ -62,16 +99,16 @@ public class DenseLayer
         for (var neuron = 0; neuron < neurons.Length; neuron++) { // running through each neuron in our neurons
             double Lneuron = biases[neuron]; // instead of adding the biases, we can directly set it as a default value
             for (var input = 0; input < inputs.Length; input++) { // running through each input for the inputs of our selected neuron
-                Lneuron += inputs[input] * weights[neuron][input];
+                Lneuron += inputs[input] * weights[input][neuron];
             }
 
-            neurons[neuron] = activate(Lneuron);
+            neurons[neuron] = Activation.activate(Lneuron);
         }
 
         return neurons;
     }
 
-    public void BackwardPass(double[] ExpectedValues, double[] inputs)
+    public void BackwardPass(double[] OutputExpectedValues, double[] inputs, double[] nextLayerDeltas, DenseHiddenLayer nextLayer)
     {
         double[][] DeltaWeights = new double[InputSize][];
         double[] DeltaBiases = new double[Size];
@@ -79,22 +116,44 @@ public class DenseLayer
 
         double[] Neurons = new double[Size];
         Neurons = ForwardPass(inputs);
-        
-        double[] errors = new double[Size];
+        double[] DeltaNeurons = new double[Size];
+        for (int neuronIndex = 0; neuronIndex < Neurons.Length; neuronIndex++)
+        {
+            double SUM = 0;
+            for (int NextLayerDeltaIndex = 0; NextLayerDeltaIndex < nextLayerDeltas.Length; NextLayerDeltaIndex++)
+            {
+                SUM += nextLayerDeltas[NextLayerDeltaIndex] * nextLayer.getLtoCWeights()[neuronIndex][NextLayerDeltaIndex];
+            }
+            
+            DeltaNeurons[neuronIndex] = Activation.derivactivate(Neurons[neuronIndex]) * SUM;
+        }
+
         for (var neuronIndex = 0; neuronIndex < Neurons.Length; neuronIndex++)
         {
-            errors[neuronIndex] = ExpectedValues[neuronIndex] - Neurons[neuronIndex];
-            // delta Neuron = Error * derivative of activation function
+            for (var input = 0; input < inputs.Length; input++)
+            {
+                DeltaWeights[input][neuronIndex] = LearningRate * DeltaNeurons[neuronIndex] * inputs[input];
+            }
+
+            DeltaBiases[neuronIndex] = LearningRate * DeltaNeurons[neuronIndex];
         }
+        
+        // return the nextLayerDeltas for the next layer before this one?
+        // return deltas?
+    }
+
+    public double[][] getLtoCWeights()
+    {
+        return weights;
     }
     
     
     
-    DenseLayer (int inputSize, int outputSize, int layerSize, double learningRate, activationFunction activationFunction)
+    DenseHiddenLayer (int inputSize, int outputSize, int layerSize, double learningRate, activationFunction activationFunction)
     {
         InputSize = inputSize;
         OutputSize = outputSize;
-        ActivationFunction = activationFunction;
+        Activation = new Activation(activationFunction);
         Size = layerSize;
         LearningRate = learningRate;
         
