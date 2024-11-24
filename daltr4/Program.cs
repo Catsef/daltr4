e55f48d6d28d44055a269fc1675989e6c0ead139
caltr4 <catieman2120@gmail.com>
2024-11-24 18:30:44 +0800

    done

--- daltr4/Program.cs
+++ daltr4/Program.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Security.Cryptography;
 using daltr4;
 
-Console.WriteLine("hello bro - Made by Caltr4 - https://github.com/Catsef/daltr4");
+Console.WriteLine("hello bro - Written by Caltr4 - https://github.com/Catsef/daltr4");
 
 public class Activation
 {
@@ -62,107 +62,184 @@ public class Activation
     }
 }
 
-public class DenseHiddenLayer
+public class Layer
 {
-    private int InputSize; // previous layer's size
-    private int OutputSize; // next layer's size
+    private bool IsHiddenLayer;
+    private bool IsFinalLayer;
+    private bool IsOutputLayer;
+    private bool IsInputLayer;
+    private int InputSize;
+    private int OutputSize;
     private int Size;
-    private double LearningRate; // tWEAKUNG SIZE
+    private double LearningRate;
     private Activation Activation;
-    private double[][] weights; // weights [Input] [Neuron]
+    private double[][] weights;
     private double[] biases;
-
+    private Nullable<double>[] inputs;
+    
     public void Initialize()
     {
-        
-        weights = new double[InputSize][]; // inputsize, size; controls how each of the inputs affect the gen value
-        for (var x = 0; x < InputSize; x++) {
-            weights[x] = new double[Size];
+        if (IsHiddenLayer || IsFinalLayer)
+        {
+            weights = new double[InputSize][]; // inputsize, size; controls how each of the inputs affect the gen value
+            for (var x = 0; x < InputSize; x++) {
+                weights[x] = new double[Size];
+                for (var i = 0; i < Size; i++)
+                {
+                    weights[x][i] = RandomNumberGenerator.GetInt32(0, 1);
+                }
+            }
             for (var i = 0; i < Size; i++)
             {
-                weights[x][i] = RandomNumberGenerator.GetInt32(0, 1);
+                if (!IsOutputLayer)
+                {
+                    biases[i] = RandomNumberGenerator.GetInt32(0, 1);
+                }
+                else
+                {
+                    break;
+                }
             }
-        }
-        biases = new double[Size]; // its the intercept - offsets values. Neuron V = (Previous Layer : Sum : ReLU * weights) + bias.
-
-        for (var i = 0; i < Size; i++)
+        } else if (IsInputLayer)
         {
-            biases[i] = RandomNumberGenerator.GetInt32(0, 1);
+            if (inputs == null)
+            {
+                throw new ArgumentException("Since you're declaring an input layer, you must provide inputs.");
+            }
+            InputSize = inputs.Length;
+        }
+    }
+
+    public double[] ForwardPass(double[] inputs)
+    {
+        if (!IsInputLayer)
+        {
+            double[] neurons = new double[Size];
+            for (var neuron = 0; neuron < neurons.Length; neuron++) { // running through each neuron in our neurons
+                double Lneuron = biases[neuron]; // instead of adding the biases, we can directly set it as a default value
+                if (IsOutputLayer)
+                {
+                    Lneuron = 0;
+                }
+                for (var input = 0; input < inputs.Length; input++) { // running through each input for the inputs of our selected neuron
+                    Lneuron += inputs[input] * weights[input][neuron];
+                }
+
+                neurons[neuron] = Activation.activate(Lneuron);
+            }
+
+            return neurons;
+        }
+        else
+        {
+            return inputs;
         }
     }
     
-    public double[] ForwardPass(double[] inputs)
+    public (double[][], double[], double[], double[]) BackwardPass(
+        double[] expectedOutputs,
+        double[] inputs,
+        double[] nextLayerDeltas,
+        Layer nextLayer)
     {
-        double[] neurons = new double[Size];
-        for (var neuron = 0; neuron < neurons.Length; neuron++) { // running through each neuron in our neurons
-            double Lneuron = biases[neuron]; // instead of adding the biases, we can directly set it as a default value
-            for (var input = 0; input < inputs.Length; input++) { // running through each input for the inputs of our selected neuron
-                Lneuron += inputs[input] * weights[input][neuron];
-            }
-
-            neurons[neuron] = Activation.activate(Lneuron);
-        }
-
-        return neurons;
-    }
-
-    public (double[][], double[], double[], double[]) 
-        BackwardPass(
-            double[] OutputExpectedValues, 
-            double[] inputs,
-            double[] nextLayerDeltas, 
-            DenseHiddenLayer nextLayer, 
-            DenseHiddenLayer prevLayer
-            )
-    {
-        double[][] DeltaWeights = new double[InputSize][];
-        for (int i = 0; i < InputSize; i++)
-        {
-            DeltaWeights[i] = new double[Size];
-        }
-        double[] DeltaBiases = new double[Size];
-        double[] DeltaInputs = new double[InputSize];
-
-        double[] Neurons = new double[Size];
-        Neurons = ForwardPass(inputs);
-        double[] DeltaNeurons = new double[Size];
-        for (int neuronIndex = 0; neuronIndex < Neurons.Length; neuronIndex++)
-        {
-            double SUM = 0;
-            for (int NextLayerDeltaIndex = 0; NextLayerDeltaIndex < nextLayerDeltas.Length; NextLayerDeltaIndex++)
-            {
-                SUM += nextLayerDeltas[NextLayerDeltaIndex] * nextLayer.getLtoCWeights()[neuronIndex][NextLayerDeltaIndex];
-            }
-            
-            DeltaNeurons[neuronIndex] = Activation.derivactivate(Neurons[neuronIndex]) * SUM;
-        }
         
-        for (var neuronIndex = 0; neuronIndex < Neurons.Length; neuronIndex++)
+        if (IsInputLayer) // if its a input
         {
-            for (var input = 0; input < inputs.Length; input++)
-            {
-                DeltaWeights[input][neuronIndex] = LearningRate * DeltaNeurons[neuronIndex] * inputs[input];
-            }
-
-            DeltaBiases[neuronIndex] = LearningRate * DeltaNeurons[neuronIndex];
+            return (null, null, null, null); // theres nothing to be tweaked. So just return everything as null
         }
-        return (DeltaWeights, DeltaBiases, DeltaInputs, DeltaNeurons);
+
+        double[][] deltaWeights = Initialize2DArray(InputSize, Size); // this will be what stores the weights that are to be changed and tweaked
+        double[] deltaBiases = biases == null ? null : new double[Size]; // biases to be changed by value to improve the layer. can be null as it might be output
+        double[] deltaNeurons = new double[Size]; // neurons that value's supposed to be changed by value
+        double[] deltaInputs = IsOutputLayer ? null : new double[InputSize]; // inputs that are to be changed? by value
+
+        double[] neurons = ForwardPass(inputs); // get the current layer's neuron output
+
+        // Compute deltas
+        if (IsOutputLayer) // if its an output layer
+        {
+            for (int i = 0; i < Size; i++) // loop through every delta neuron
+            {
+                deltaNeurons[i] = (expectedOutputs[i] - neurons[i]) * Activation.derivactivate(neurons[i]); // change it to equal to the distance between expected and real
+            }
+        }
+        else // if not
+        {
+            for (int i = 0; i < Size; i++) // loop through inputs
+            {
+                double sum = 0;
+                for (int j = 0; j < nextLayer.Size; j++) // loop through the neurons
+                {
+                    sum += nextLayerDeltas[j] * nextLayer.weights[i][j]; // add to the sum of the differences
+                }
+                deltaNeurons[i] = sum * Activation.derivactivate(neurons[i]); 
+            }
+        }
+
+        // Update weights and biases
+        for (int i = 0; i < InputSize; i++) 
+        {
+            for (int j = 0; j < Size; j++)
+            {
+                deltaWeights[i][j] = LearningRate * deltaNeurons[j] * inputs[i]; // to be applied when learn function
+            }
+        }
+
+        if (deltaBiases != null)
+        {
+            for (int j = 0; j < Size; j++)
+            {
+                deltaBiases[j] = LearningRate * deltaNeurons[j]; // to be applied when learn function
+            }
+        }
+
+        // Compute deltaInputs for hidden layers
+        if (deltaInputs != null)
+        {
+            for (int i = 0; i < InputSize; i++)
+            {
+                double sum = 0;
+                for (int j = 0; j < Size; j++)
+                {
+                    sum += deltaNeurons[j] * weights[i][j]; // negative gradient for neuron I in current layerL the sum of everything in J {negative gradient for neuron J in previous layer times the weight connecting neuron i to neuron J.
+                }
+                deltaInputs[i] = sum;
+            }
+        }
+
+        return (deltaWeights, deltaBiases, deltaInputs, deltaNeurons);
     }
+
+    private double[][] Initialize2DArray(int rows, int cols)
+    {
+        double[][] array = new double[rows][];
+        for (int i = 0; i < rows; i++)
+        {
+            array[i] = new double[cols];
+        }
+        return array;
+    }
+
 
     public double[][] getLtoCWeights()
     {
         return weights;
     }
     
-    
-    
-    DenseHiddenLayer (int inputSize, int outputSize, int layerSize, double learningRate, activationFunction activationFunction)
+    Layer (bool isHiddenLayer, bool isFinalLayer, bool isOutputLayer, 
+        bool isInputLayer, int inputSize, int outputSize, 
+        double learningRate, activationFunction activation, int layerSize, 
+        int layerBias, Nullable<double>[] inputs)
     {
+        IsHiddenLayer = isHiddenLayer;
+        IsFinalLayer = isFinalLayer;
+        IsOutputLayer = isOutputLayer;
+        IsInputLayer = isInputLayer;
         InputSize = inputSize;
         OutputSize = outputSize;
-        Activation = new Activation(activationFunction);
+        Activation = new Activation(activation);
         Size = layerSize;
         LearningRate = learningRate;
-        
     }
 }
+
---
+++ daltr4/RewrittenCNN.cs
@@ -0,0 +1,272 @@
+
+
+// Backpropagation algorithm:
+// Inputs all arrive through the preinitialised paths
+// weights are chosen randomly
+// Calculate the output of each neuron from the input layer
+// to the hidden layer to the output layer
+// Calculate the error in the output layer
+// From the output layer, go back to the hidden layer to adjust the weights
+// so that it reduces the error
+// Repeat the process until the desired output is achieved.
+
+// Initialise weights to small random values
+// While we're not stopping the stepping, do the following
+// For each training pair (data) do the following
+// Calculate and populate the output by sending and activating
+// Each output unit "OutputUnitK"
+// (K is index of unit from 1 to the amount of outputs N)
+// recieves a target output corresponding to an given input
+// and the error between the real output and the target output
+// is calculated as 
+// Error K = (Target Output K - Real Output K)
+// Each neuron in the hidden layer (TotalLayers - 1) as J sums
+// basic example
+// In a classifier model, we have 5 neurons in the output layer.
+// we will call this layer L. the softmax value of each output
+// neuron represents the likelihood out of 1 that the input
+// belongs to their category.
+// For now, we'll say that the output unit is representing the perfect
+// and correct prediction, which we'll call L<c>.
+// L<c>'s activation function is a composite function, containing
+// the many nested activation functions of the entire neural network
+// from the input to the output.
+// Minimizing the loss would entail making adjustments throughout the
+// network that bring the output of L<c>'s error closer to 0.
+// to do so, we'll need to know how any change in previous layers will
+// change L<c>'s output, which means that
+// we will have to find the partial derivatives of L<c>'s activation
+// function.
+
+using daltr4;
+using MathNet.Numerics;
+
+public class ActivationUtility
+{
+    private activationFunction ActivationFunction;
+    
+    public ActivationUtility(activationFunction func)
+    {
+        ActivationFunction = func;
+    }
+    
+    double ReLU (double x)
+    {
+        return Math.Max(0, x);
+    }
+    double Tanh(double x)
+    {
+        return 2/(1+Math.Pow(Math.E, -(2*x)))-1;
+    }
+    private double BipolarSigmoid(double x)
+    {
+        return (1-Math.Exp(-x))/(1+Math.Exp(-x));
+    }
+
+    private double UnReLU(double x)
+    {
+        if (x < 0) return 0;
+        else return 1;
+    }
+
+    private double UnTanh(double x)
+    {
+        double e = Math.E;
+        return 1 - Math.Pow(Math.Exp(x) - Math.Exp(-x), 2) / Math.Pow(Math.Exp(x) + Math.Exp(-x), 2);
+    }
+
+    private double UnBipolarSigmoid(double x)
+    {
+        return 1 - Math.Pow(Tanh(x / 2), 2);
+    }
+    public double activate(double x)
+    {
+        if (ActivationFunction == activationFunction.ReLu) return ReLU(x);
+        if (ActivationFunction == activationFunction.Tanh) return Tanh(x);
+        if (ActivationFunction == activationFunction.Sigmoid) return BipolarSigmoid(x);
+        else return 0;
+    }
+
+    public double derivactivate(double x)
+    {
+        if (ActivationFunction == activationFunction.ReLu) return UnReLU(x);
+        if (ActivationFunction == activationFunction.Tanh) return UnTanh(x);
+        if (ActivationFunction == activationFunction.Sigmoid) return UnBipolarSigmoid(x);
+        else return 0;
+    }
+}
+class DenseLayer
+{
+    public int amountOfNeurons;
+    
+    public bool isInputLayer;
+    public bool isOutputLayer;
+    public bool isDenseLayer;
+    public double[][]? weights; // -o. connections from the layer behind. [BEHIND], [CURRENT]
+    public double[]? biases; // +bias
+    public double[]? inputs;
+    public double? learningRate;
+    public double[]? input;
+    public ActivationUtility activation;
+
+    public bool UpdateInput(double[] input)
+    {
+        if (!isInputLayer) return false;
+        this.input = input;
+        return true;
+    }
+    
+    
+    public double[] ForwardPass(double[]? lastLayerNeurons)
+    {
+        if ((isDenseLayer || isOutputLayer) && lastLayerNeurons == null) 
+        {
+            throw new ArgumentException("Must pass last layer neurons to forward pass!");
+        } else if (isInputLayer)
+        {
+            return input;
+        }
+        double[] Neurons = new double[amountOfNeurons];
+        for (var NeuronIndex = 0; NeuronIndex < Neurons.Length; NeuronIndex++)
+        {
+            var Neuron = biases[NeuronIndex]; // add the bias
+            if (isOutputLayer)
+            {
+                // if its the output layer, no bias should be added
+                Neuron = 0;
+            }
+
+            for (var LastLayerNeuronIndex = 0;
+                 LastLayerNeuronIndex < lastLayerNeurons.Length;
+                 LastLayerNeuronIndex++)
+            {
+                // the sum of all previous layer neurons times the weight
+                Neuron += lastLayerNeurons[LastLayerNeuronIndex] * weights[LastLayerNeuronIndex][NeuronIndex]; // j-i
+            }
+            // put through the activation function
+            Neuron = activation.activate(Neuron);
+            Neurons[NeuronIndex] = Neuron;
+
+        }
+        return Neurons;
+    }
+
+    public (double[][], double[]) BackwardPass(double[] ExpectedOutputOrNextErrorTerms, double[] Input, DenseLayer? NextLayer, DenseLayer? PreviousLayer)
+    {
+        if (isOutputLayer)
+        {
+            if (PreviousLayer == null)
+            {
+                throw new ArgumentException("Must pass previous layer to backward pass if output!");
+                return (null, null);
+            }
+            double[] GottenOutputs = new double[amountOfNeurons];
+            GottenOutputs = ForwardPass(Input);
+            double[] ErrorTermsForOutput = new double[amountOfNeurons];
+            if (ExpectedOutputOrNextErrorTerms.Length != ErrorTermsForOutput.Length)
+            {
+                throw new Exception("Expected output length needs to fit to output node length!");
+                return (null, null);
+            }
+            for (var OutputIndex = 0; OutputIndex < ErrorTermsForOutput.Length; OutputIndex++)
+            {
+                ErrorTermsForOutput[OutputIndex] = (ExpectedOutputOrNextErrorTerms[OutputIndex] - GottenOutputs[OutputIndex])
+                                                   * activation.derivactivate(GottenOutputs[OutputIndex]);
+            }
+
+            return (null, ErrorTermsForOutput);
+        } else if (isDenseLayer)
+        {
+            if (NextLayer == null)
+            {
+                throw new ArgumentException("Must pass next layer to backward pass!");
+                return (null, null);
+            }
+            double[] GottenOutputs = new double[amountOfNeurons];
+            GottenOutputs = ForwardPass(Input);
+            
+            double[] ErrorTermsForCurrentLayer = new double[amountOfNeurons];
+            for (var CurrentNeuronIndex = 0; CurrentNeuronIndex < amountOfNeurons; CurrentNeuronIndex++)
+            {
+                double SumOfAllErrorsOfNextLayer = 0;
+                for (var CurrentNextErrorIndex = 0; CurrentNextErrorIndex < ExpectedOutputOrNextErrorTerms.Length; CurrentNextErrorIndex++)
+                {
+                    SumOfAllErrorsOfNextLayer += ExpectedOutputOrNextErrorTerms[CurrentNextErrorIndex] *
+                                                 NextLayer.weights[CurrentNeuronIndex][CurrentNextErrorIndex];
+                }
+                SumOfAllErrorsOfNextLayer = activation.derivactivate(SumOfAllErrorsOfNextLayer);
+                ErrorTermsForCurrentLayer[CurrentNeuronIndex] = SumOfAllErrorsOfNextLayer;
+            }
+            
+            double[][] WeightChanges = new double[Input.Length][];
+            for (var i=0;i<Input.Length;i++)
+            {
+                WeightChanges[i] = new double[amountOfNeurons];
+            }
+            
+            for (var neuron = 0; neuron < amountOfNeurons; neuron++)
+            {
+                for (var input = 0; input < Input.Length; input++)
+                {
+                    WeightChanges[input][neuron] = (double)(-learningRate * ErrorTermsForCurrentLayer[neuron] * Input[input]);
+                }
+                biases[neuron] = (double) (-learningRate * ErrorTermsForCurrentLayer[neuron]);
+            }
+            
+            return (WeightChanges, ErrorTermsForCurrentLayer);
+        } else if (isInputLayer)
+        {
+            return (null, null);
+        }
+
+        return (null, null);
+    }
+
+    public void updateWeights(double[][] weights, double[] Grad)
+    {
+        
+    }
+
+    public DenseLayer(bool input, bool output, bool dense, int neurons, activationFunction function, double? learningRate, double[]? inputs)
+    {
+        if (isDenseLayer && learningRate == null)
+        {
+            learningRate = 0;
+            Console.WriteLine("Dense layer needs Learning rate to be set! returning to 0.");
+            
+        }
+
+        if (!isInputLayer && inputs != null)
+        {
+            throw new ArgumentException("Dense or output layers don't need inputs!");
+        } else if (isInputLayer && inputs == null)
+        {
+            throw new ArgumentException("Input layers need inputs!");
+        }
+        else
+        {
+            this.input = inputs;
+        } 
+        activation = new ActivationUtility(function);
+        isInputLayer = input;
+        isOutputLayer = output;
+        isDenseLayer = dense;
+        amountOfNeurons = neurons;
+        
+    }
+}
+
+class CNN
+{
+    public DenseLayer[] layers;
+    public DenseLayer InputLayer;
+    public DenseLayer OutputLayer;
+
+    public CNN(DenseLayer[] layers, double learningRate, double[] defaultInputs)
+    {
+        if (!layers[0].isInputLayer) throw new ArgumentException("1st layer must be input layer!");
+        if (!layers[-1].isOutputLayer) throw new ArgumentException("Last layer must be output layer!");
+        
+        
+    }
+}
